52db5283ec842ae0972b80fd07942633a7bfc88c
Scott Prue <prescottprue@gmail.com>
2015-08-04 16:42:05 -0400

    Endpoint documentation generated with apidoc to /docs in public folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,5 +66,6 @@ module.exports = function(grunt){
 		}
 	});
 	require('load-grunt-tasks')(grunt);
+	grunt.registerTask('docs', ['apidoc'])
 	grunt.registerTask('default', ['shell:mongodb', 'nodemon']);
 };
